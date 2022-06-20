b4f00dbacd8863efb24858a493ddaa50ad92aab0
root <ashim.anwar22@gmail.com>
2022-06-20 08:44:44 +0000

    RMX1901: Switched To Neutron Clang 14

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,11 @@
-echo 'Cloning Vendor tree [1/2]'
+echo 'Cloning Vendor tree [1/3]'
 # Vendor Tree
 git clone git@github.com:ashim-anwar/vendor_realme_RMX1901-12.git -b 12.1 vendor/realme/RMX1901
 
-echo 'Cloning Kernel tree [2/2]'
+echo 'Cloning Kernel tree [2/3]'
 # Kernel Tree
 git clone git@github.com:ashim-anwar/kernel_realme_RMX1901-12.git -b 12.1 kernel/realme/RMX1901
 
+echo 'Cloning Neutron Clang [3/3]'
+# Neutron Clang
+git clone --depth=1 https://gitlab.com/dakkshesh07/neutron-clang.git -b Neutron-14 prebuilts/clang/host/linux-x86/clang-neutron
