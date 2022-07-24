0cc63c0014b2b049fb36753790b56a82733ddbfd
Himjyoti Thakuria <92359223+H1mJT@users.noreply.github.com>
2022-07-24 16:40:07 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,4 +13,4 @@ git clone --depth=1 https://gitlab.com/dakkshesh07/neutron-clang.git -b Neutron-
 
 echo 'Patching Realme In-Screen Proximity [4/4]'
 # Realme In-Screen Proximity Patch
-cd frameworks/base && git fetch git@github.com:H1mJT/proximity_patch.git -b rice && git cherry-pick 198b3fa983482e67cc10f91debc72710ee831404 && cd ../..
+cd frameworks/base && git fetch git@github.com:H1mJT/proximity_patch.git rice && git cherry-pick 198b3fa983482e67cc10f91debc72710ee831404 && cd ../..
