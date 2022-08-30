2a58618819e1a9714c6e2c3c049642a593206150
H1mJT <himjjyoti22@gmail.com>
2022-08-30 07:11:16 -0400

    Refactored vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,19 +4,16 @@ git clone git@github.com:H1mJT/vendor_realme_RMX1901.git -b 12.1 vendor/realme/R
 
 echo 'Cloning Kernel tree [2/4]'
 # Kernel Tree
-git clone git@github.com:H1mJT/kernel_realme_RMX1901.git -b 12.1 kernel/realme/RMX1901
-
-echo 'Cloning Neutron Clang [3/4]'
-# Neutron Clang
-git clone --depth=1 https://gitlab.com/jjpprrrr/prelude-clang prebuilts/clang/host/linux-x86/clang-prelude
+git clone git@github.com:H1mJT/kernel_realme_RMX1901.git -b upstream kernel/realme/RMX1901
 
 
-echo 'Patching Realme In-Screen Proximity [4/4]'
+
+echo 'Patching Realme In-Screen Proximity [3/4]'
 # Realme In-Screen Proximity Patch
 cd frameworks/base && git fetch git@github.com:H1mJT/proximity_patch.git && git cherry-pick afb38a0f9f9a612b13b12b901ab9b7f0fffae3de && cd ../..
 
 
 # Realme Dirac
-echo 'Adding Realme Dirac Support [6/6]'
+echo 'Adding Realme Dirac Support [4/4]'
 git clone https://github.com/techyminati/android_packages_apps_RealmeDirac.git -b lineage-19.1 packages/apps/RealmeDirac
 
