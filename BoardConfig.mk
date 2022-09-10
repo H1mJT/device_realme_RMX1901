f9b1c4ed51be6dbe9cbafb962beac8b5ddf61fa6
H1mJT <himjjyoti22@gmail.com>
2022-09-10 12:01:22 -0400

    Switch back to AOSP clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,8 +46,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/realme/RMX1901
 TARGET_KERNEL_CONFIG := RMX1901_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := proton
-KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton
+
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm710
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,20 +1,17 @@
-echo 'Cloning Vendor tree [1/5]'
+echo 'Cloning Vendor tree [1/4]'
 # Vendor Tree
 git clone git@github.com:H1mJT/vendor_realme_RMX1901.git -b rice vendor/realme/RMX1901
 
-echo 'Cloning Kernel tree [2/5]'
+echo 'Cloning Kernel tree [2/4]'
 # Kernel Tree
 git clone git@github.com:H1mJT/kernel_realme_RMX1901.git -b upstream kernel/realme/RMX1901
 
-echo 'Cloning Proton Clang [3/5]'
-# Proton Clang
-git clone --depth=1 https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/clang-proton
 
 # Realme Dirac
-echo 'Adding Realme Dirac Support [4/5]'
+echo 'Adding Realme Dirac Support [3/4]'
 git clone https://github.com/techyminati/android_packages_apps_RealmeDirac.git -b lineage-19.1 packages/apps/RealmeDirac
 
-echo 'Patching Realme In-Screen Proximity [5/5]'
+echo 'Patching Realme In-Screen Proximity [4/4]'
 # Realme In-Screen Proximity Patch
 cd frameworks/base && git fetch git@github.com:H1mJT/proximity_patch.git && git cherry-pick de630e735cdbb68a606d3bbbf8b1674be31bab2d && cd ../..
 
